7cd8954a3aae55449a94b99b4548153431349e86
Pedro Cattori <pcattori@gmail.com>
2022-03-10 22:24:20 -0600

    feat: format output js with prettier

--- untype.js
+++ untype.js
@@ -1,4 +1,5 @@
 const babel = require("@babel/core")
+const prettier = require('prettier')
 
 function untype(filename, ts) {
   const { code: js } = babel.transformSync(ts, {
@@ -14,18 +15,10 @@ function untype(filename, ts) {
   });
 
   /*
-   TODO:
-   Babel `compact`/`retainLines` are both imperfect at formatting code.
-  
-   We could lint-fix or format the js at this point,
-   but users who care about that sort of stuff should set up their own lint/formatting
-   and run `--fix` or `--write` themselves.
-
-   Though might be nice to provide an output that at least _we_ think looks good...
-   */
-
-  // For now, just replace multiple blank lines with a single one
-  return js.replace(/\n\s*\n\s*\n/g, '\n\n')
+   Babel's `compact` and `retainLines` options are both bad at formatting code.
+   Use Prettier for nicer formatting.
+  */
+  return prettier.format(js, { parser: 'babel-ts'})
 }
 
 module.exports = {
